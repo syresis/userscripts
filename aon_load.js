52ebff3052f149598640313a77bc9346537fe72c
Syresis <>
2018-09-14 21:16:40 -0400

    Added aon_start & aon_load

---
+++ aon_load.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Archives of Nethys Improvements On Load
+// @match        http://aonprd.com/*
+// @grant    GM_addStyle
+// @run-at   document-load
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var images = document.getElementsByTagName("img");
+
+    for (var i in images) {
+      var image = images[i];
+
+        if (image.src === "http://aonprd.com/Images/heading_flourish.png") {
+           image.style["-webkit-filter"] = "invert(100%)";
+        }
+    }
+    // Your code here...
+})();
---
+++ aon_start.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         Archives of Nethys Improvements On Start
+// @match        http://aonprd.com/*
+// @grant    GM_addStyle
+// @run-at   document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+})();
+
+GM_addStyle ( `
+    #wrapper {
+        width: 100%;
+    }
+
+    body.light {
+        background: #0f0f0f;
+    }
+
+    #main-menu {
+        background: none;
+        padding: 0 0 0 0;
+        width: 9%;
+        top: 20%;
+
+        position: fixed !important;
+    }
+
+    #main-menu, #main-menu a:hover {
+        color: #cbc18f;
+    }
+
+    #main-wrapper {
+        padding-left: 45px;
+        margin-left: 10%;
+    }
+
+    #main-menu, #main-menu a {
+        color: #dedede;
+        font-family: "Century Gothic", CenturyGothic, AppleGothic, sans-serif;
+    }
+` );
